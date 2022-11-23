03f18844f22faa89773bca93404365c40088c66b
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2022-11-23 13:06:44 +0530

    fix: Issue with POST request on deployment

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 // See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
+const clone = require('clone')
+const data = require('./db.json')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+
+// For mocking the POST request, POST request won't make any changes to the DB
+server.use((req, res, next) => {
+    if (req.path === '/') return next()
+    router.db.setState(clone(data))
+    next()
+})
+
 server.use(router)
 server.listen(process.env.PORT || 8000, () => {
     console.log('JSON Server is running')
